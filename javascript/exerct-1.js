775f94acc93c463f3057f43baf40ed3286f9a99d
Mikelemag <mikelepinto2017@gmail.com>
2025-04-11 11:53:16 -0300

    aprendendo javascript

---
+++ javascript/exerct-1.js
@@ -0,0 +1,14 @@
+const nome = "Mikele" //texto - string
+console.log (nome)
+const idade = 28 // number
+console.log (idade)
+const signo = "cancer"
+console.log (signo)
+const altura = 1.57
+const maiorDeIdade = true
+console.log(" Olá seu nome é " + nome + ", sua idade é " + idade + ", e seu signo é " + signo + ". Sua altura é " + altura )
+console.log(" Voce é maior de idade? " + maiorDeIdade)
+
+//template string ou string dinamica
+const frase =`Ola seu nome é ${nome}, e voce tem ${idade}`
+console.log(frase)
---
+++ javascript/exerct-5.js
@@ -0,0 +1,12 @@
+//função recebera dias alugados
+//o valor por dia é 250,00
+
+function calcularAluguel(dias){
+  const valorPorDia = 250
+
+  const calculo = dias * valorPorDia
+
+  console.log(calculo)
+
+}
+calcularAluguel(10)
